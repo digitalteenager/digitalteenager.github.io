adbf32236ca1a046fba82cbf419e6f5420845c00
digitalteenager <51023433+digitalteenager@users.noreply.github.com>
2019-05-25 18:03:40 +0500

    Fishingbase

---
+++ slidejs.js
@@ -0,0 +1,19 @@
+$('.carousel').slick({
+	arrow:false,
+	autoplay:true,
+	autoplaySpeed: 7000,
+	speed: 1700,
+	pauseOnFocus:false,
+	pauseOnHover:false,
+
+});
+$('.reviews-slide').slick({
+	arrow:false,
+	dots:true,
+	autoplay:true,
+	autoplaySpeed: 7000,
+	speed: 1700,
+	pauseOnFocus:false,
+	pauseOnHover:false,
+
+});
